db53a89bb1d13f4b74ce56e18dcb0597ca8693fb
senze17 <69090845+senze17@users.noreply.github.com>
2023-10-24 11:24:08 +0200

    Program

---
+++ JuleProjekt/JuleProjekt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JuleProjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
